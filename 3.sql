6f157e5c898e11089f7d0f1a1abb6e14662ba034
Stevanus Setiawan <stevsetiawann@gmail.com>
2020-07-31 22:32:52 +0700

    Create 3.sql

---
+++ 3.sql
@@ -0,0 +1,13 @@
+Syntax:
+
+INSERT INTO table_name (col1, col2, col3, ...)
+VALUES (value1, value2, value3, ...); 
+
+
+INSERT INTO penyanyi(nama) VALUES ('Afgan');
+INSERT INTO penyanyi(nama) VALUES ('Raisa');
+INSERT INTO penyanyi(nama) VALUES ('Isyana Sarasvati');
+INSERT INTO penyanyi(nama) VALUES ('Tulus');
+INSERT INTO penyanyi(nama) VALUES ('Vidi Aldiano');
+
+
