ed6fcfa32f2da69af28560bc4ea1311da9356f28
Stevanus Setiawan <stevsetiawann@gmail.com>
2020-08-01 22:54:47 +0700

    Update 3.sql

--- 3.sql
+++ 3.sql
@@ -10,4 +10,14 @@ INSERT INTO penyanyi(nama) VALUES ('Isyana Sarasvati');
 INSERT INTO penyanyi(nama) VALUES ('Tulus');
 INSERT INTO penyanyi(nama) VALUES ('Vidi Aldiano');
 
+INSERT INTO album(nama,tahun_rilis, jumlah_lagu, penyanyi_id) VALUES ('Confession No.1',2008, 13, 1);
+INSERT INTO album(nama,tahun_rilis, jumlah_lagu, penyanyi_id) VALUES ('L1ve to Love, Love to L1ve',2013, 10, 1);
+INSERT INTO album(nama,tahun_rilis, jumlah_lagu, penyanyi_id) VALUES ('Raisa',2011, 9, 2);
+INSERT INTO album(nama,tahun_rilis, jumlah_lagu, penyanyi_id) VALUES ('Explore!',2015, 10, 3);
+INSERT INTO album(nama,tahun_rilis, jumlah_lagu, penyanyi_id) VALUES ('Paradox',2017, 10, 3);
+INSERT INTO album(nama,tahun_rilis, jumlah_lagu, penyanyi_id) VALUES ('Tulus',2011, 10, 4);
+INSERT INTO album(nama,tahun_rilis, jumlah_lagu, penyanyi_id) VALUES ('Gajah',2014, 9, 4);
+INSERT INTO album(nama,tahun_rilis, jumlah_lagu, penyanyi_id) VALUES ('Pelangi di Malam Hari',2008, 11, 5);
+INSERT INTO album(nama,tahun_rilis, jumlah_lagu, penyanyi_id) VALUES ('Dunia Baru',2013, 6, 5);
+
 
