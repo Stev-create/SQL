bd53c482ee1efa597fc68e0703fdccc8e5469c3f
Stevanus Setiawan <stevsetiawann@gmail.com>
2020-08-04 01:54:44 +0700

    Update 3.sql

--- 3.sql
+++ 3.sql
@@ -20,4 +20,30 @@ INSERT INTO album(nama,tahun_rilis, jumlah_lagu, penyanyi_id) VALUES ('Gajah',20
 INSERT INTO album(nama,tahun_rilis, jumlah_lagu, penyanyi_id) VALUES ('Pelangi di Malam Hari',2008, 11, 5);
 INSERT INTO album(nama,tahun_rilis, jumlah_lagu, penyanyi_id) VALUES ('Dunia Baru',2013, 6, 5);
 
-
+INSERT INTO songs(judul,album_id) VALUES ('Terima Kasih Cinta', 1);
+INSERT INTO songs(judul,album_id) VALUES ('Sadis', 1);
+INSERT INTO songs(judul,album_id) VALUES ('My Confession', 1);
+INSERT INTO songs(judul,album_id) VALUES ('Katakan Tidak', 2);
+INSERT INTO songs(judul,album_id) VALUES ('Jodoh Pasti Bertemu', 2);
+INSERT INTO songs(judul,album_id) VALUES ('Sabar', 2);
+INSERT INTO songs(judul,album_id) VALUES ('Serba Salah', 3);
+INSERT INTO songs(judul,album_id) VALUES ('Apalah (Arti Menunggu)', 3);
+INSERT INTO songs(judul,album_id) VALUES ('Terjebak Nostalgia', 3);
+INSERT INTO songs(judul,album_id) VALUES ('Keep Being You', 4);
+INSERT INTO songs(judul,album_id) VALUES ('Tetap Dalam Jiwa', 4);
+INSERT INTO songs(judul,album_id) VALUES ('Kau Adalah', 4);
+INSERT INTO songs(judul,album_id) VALUES ('Terpesona', 5);
+INSERT INTO songs(judul,album_id) VALUES ('Anganku Anganmu', 5);
+INSERT INTO songs(judul,album_id) VALUES ('Sekali Lagi', 5);
+INSERT INTO songs(judul,album_id) VALUES ('Teman Pesta', 6);
+INSERT INTO songs(judul,album_id) VALUES ('Sewindu', 6);
+INSERT INTO songs(judul,album_id) VALUES ('Teman Hidup', 6);
+INSERT INTO songs(judul,album_id) VALUES ('Baru', 7);
+INSERT INTO songs(judul,album_id) VALUES ('Sepatu', 7);
+INSERT INTO songs(judul,album_id) VALUES ('Gajah', 7);
+INSERT INTO songs(judul,album_id) VALUES ('Nuansa Bening', 8);
+INSERT INTO songs(judul,album_id) VALUES ('Cinta Jangan Kau Pergi', 8);
+INSERT INTO songs(judul,album_id) VALUES ('Pelangi di Malam Hari', 8);
+INSERT INTO songs(judul,album_id) VALUES ('Di Bawah Langit Ibukota', 9);
+INSERT INTO songs(judul,album_id) VALUES ('Pupus/Kasih Tak Sampai (mash-up)', 9);
+INSERT INTO songs(judul,album_id) VALUES ('Apakah Ku Jatuh Cinta', 9);
